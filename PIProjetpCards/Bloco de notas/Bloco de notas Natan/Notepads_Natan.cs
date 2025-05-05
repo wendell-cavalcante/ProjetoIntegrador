04d73387750965023cfab74bfddce5ef69248d4d
natanaelvs <vnatan911@gmail.com>
2025-05-05 10:05:21 -0300

    Wendell veja se vai dar tudo certo no pull

---
+++ PIProjetpCards/Bloco de notas/Bloco de notas Natan/Notepads_Natan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIProjetpCards.Bloco_de_notas.Bloco_de_notas_Natan
+{
+    public partial class Notepads_Natan : UserControl
+    {
+        public Notepads_Natan()
+        {
+            InitializeComponent();
+        }
+    }
+}
