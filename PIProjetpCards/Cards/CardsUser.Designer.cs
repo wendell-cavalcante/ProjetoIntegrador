f0f59802a6d2ab5e4eca21d2a1297c055578dd75
Wendell Oliveira <wendell160326@gmail.com>
2025-05-09 20:49:14 -0300

    Add files via upload

---
+++ PIProjetpCards/Cards/CardsUser.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PIProjetpCards.Cards
+{
+    partial class CardsUser
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CardsUser));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNameCard = new System.Windows.Forms.TextBox();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaveCard = new System.Windows.Forms.Button();
+            this.categorieComboBox = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 18F);
+            this.label6.Location = new System.Drawing.Point(219, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 29);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Pergunta:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(219, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 29);
+            this.label5.TabIndex = 54;
+            this.label5.Text = "Nome do cartão:";
+            // 
+            // txtNameCard
+            // 
+            this.txtNameCard.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNameCard.Location = new System.Drawing.Point(223, 169);
+            this.txtNameCard.Multiline = true;
+            this.txtNameCard.Name = "txtNameCard";
+            this.txtNameCard.Size = new System.Drawing.Size(173, 21);
+            this.txtNameCard.TabIndex = 53;
+            this.txtNameCard.Enter += new System.EventHandler(this.txtNameCard_Enter);
+            this.txtNameCard.Leave += new System.EventHandler(this.txtNameCard_Leave);
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtQuestion.Location = new System.Drawing.Point(224, 302);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtQuestion.Size = new System.Drawing.Size(243, 85);
+            this.txtQuestion.TabIndex = 49;
+            this.txtQuestion.Enter += new System.EventHandler(this.txtQuestion_Enter);
+            this.txtQuestion.Leave += new System.EventHandler(this.txtQuestion_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 18F);
+            this.label3.Location = new System.Drawing.Point(498, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 29);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "Categoria:";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAnswer.Location = new System.Drawing.Point(503, 302);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAnswer.Size = new System.Drawing.Size(243, 85);
+            this.txtAnswer.TabIndex = 47;
+            this.txtAnswer.Enter += new System.EventHandler(this.txtAnswer_Enter);
+            this.txtAnswer.Leave += new System.EventHandler(this.txtAnswer_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 18F);
+            this.label2.Location = new System.Drawing.Point(498, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 29);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Resposta:";
+            // 
+            // btnSaveCard
+            // 
+            this.btnSaveCard.BackColor = System.Drawing.Color.Indigo;
+            this.btnSaveCard.FlatAppearance.BorderSize = 0;
+            this.btnSaveCard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveCard.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveCard.ForeColor = System.Drawing.Color.White;
+            this.btnSaveCard.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveCard.Image")));
+            this.btnSaveCard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSaveCard.Location = new System.Drawing.Point(220, 426);
+            this.btnSaveCard.Name = "btnSaveCard";
+            this.btnSaveCard.Size = new System.Drawing.Size(526, 52);
+            this.btnSaveCard.TabIndex = 44;
+            this.btnSaveCard.Text = "Salvar Cartão";
+            this.btnSaveCard.UseVisualStyleBackColor = false;
+            this.btnSaveCard.Click += new System.EventHandler(this.btnSaveCard_Click);
+            // 
+            // categorieComboBox
+            // 
+            this.categorieComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.categorieComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.categorieComboBox.FormattingEnabled = true;
+            this.categorieComboBox.Location = new System.Drawing.Point(503, 169);
+            this.categorieComboBox.Name = "categorieComboBox";
+            this.categorieComboBox.Size = new System.Drawing.Size(173, 21);
+            this.categorieComboBox.TabIndex = 59;
+            this.categorieComboBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-147, -96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1264, 817);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 61;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(56)))), ((int)(((byte)(192)))));
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
+            this.btnBack.Location = new System.Drawing.Point(68, 70);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(24, 24);
+            this.btnBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnBack.TabIndex = 85;
+            this.btnBack.TabStop = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click_1);
+            // 
+            // CardsUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.categorieComboBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNameCard);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSaveCard);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "CardsUser";
+            this.Size = new System.Drawing.Size(988, 616);
+            this.Load += new System.EventHandler(this.CardsUser_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNameCard;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSaveCard;
+        private System.Windows.Forms.ComboBox categorieComboBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox btnBack;
+    }
+}
